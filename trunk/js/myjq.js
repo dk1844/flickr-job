879e36e41ba48b46dbf4157266493a6e8f0e3396
dk1844 <dk1844@a68aa6fc-1603-ab3b-90bc-dd4c2b731ba9>
2010-11-25 14:39:14 +0000

    search by date implemented, jquery for this! (tak jeste jednou..)

--- trunk/js/myjq.js
+++ trunk/js/myjq.js
@@ -13,7 +13,7 @@ function rerank_disable(how){
         });
     }
     
-//$("rerank_cb").click();
+    //$("rerank_cb").click();
 
 }
 
@@ -26,7 +26,7 @@ function rerank_enable(how) {
     $("#rr_fieldset").attr("class", "expanded")
     $("#rr_details input, #rr_details select").removeAttr('disabled');
     disable_all_rerank_details(); //fixes unchanged value from the last time!
-//$("rerank_cb").click();
+    //$("rerank_cb").click();
 
 }
 
@@ -51,7 +51,7 @@ function disable_all_rerank_details(how){
                 $(detail_name).fadeIn(1000);
             }
 
-             //alert("disRem:" + detail_name);
+            //alert("disRem:" + detail_name);
            
         } else {
             //hide it
@@ -75,7 +75,7 @@ function my_date_input_extend() {
     $.extend(DateInput.DEFAULT_OPTS, {
         stringToDate: function(string) {
             var matches;
-                                    // for czech-alike date: 2.12.2011
+            // for czech-alike date: 2.12.2011
             if (matches = string.match(/^(\d{1,2})\.(\d{1,2})\.(\d{4,4})$/)) {
                 return new Date(matches[3], matches[2] - 1, matches[1]);
             } else {
@@ -86,8 +86,8 @@ function my_date_input_extend() {
         dateToString: function(date) {
             var month = (date.getMonth() + 1).toString();
             var dom = date.getDate().toString();
-            if (month.length == 1) month = "0" + month;
-            if (dom.length == 1) dom = "0" + dom;
+            //if (month.length == 1) month = "0" + month;
+            //if (dom.length == 1) dom = "0" + dom;
             return  dom + "." + month + "." + date.getFullYear();
         }
     });
@@ -95,45 +95,108 @@ function my_date_input_extend() {
 }
 
 
+function disable_date(which, how) {
+    if (which != "from") which = "to";
+    var block_id = "input_date_" + which + "_detail"; //as in input_date_from_detail;
 
-$(document).ready(function(){
-
-    //set init;
-    if (!$('#rerank_cb').attr("checked")) {
-        rerank_disable();
+    $("#" + block_id + " input").attr('disabled','disabled');
+    if(how != "fade") {
+        $("#" + block_id).hide();
+    } else {
+        $("#" + block_id).fadeOut(1000);
     }
-    //details hide
-    disable_all_rerank_details();
+}
 
-    //setup date input;
-    my_date_input_extend();
-    $(".date_input").date_input();
+function enable_date(which, how) {
+    if (which != "from") which = "to";
+    var block_id = "input_date_" + which + "_detail"; //as in input_date_from_detail;
+
+    $("#" + block_id + " input").removeAttr('disabled');
+    if(how != "fade") {
+        $("#" + block_id).show();
+    } else {
+        $("#" + block_id).fadeIn(1000);
+    }
+}
 
 
+function hide_show_dates(chosentype, how) {
+    // input_date_types form jsHelper php (generated)
+ //alert(chosentype);
+    switch(chosentype)
+    {
+        //no dates at al
+        default:
+        case input_date_types[0]:
+            disable_date("from", how);
+            disable_date("to", how);
+            break;
 
+        case input_date_types[1]: //from_only
+            enable_date("from", how);
+            disable_date("to", how);
+            break;
 
+        case input_date_types[2]: //from_only
+            disable_date("from", how);
+            enable_date("to", how);
+            break;
 
-    //----------functions----------------------
-  
-    $('#rerank_cb').change(function () {
-        if ($('#rerank_cb').attr("checked")) {
-            rerank_enable("fade");
-            return;
+        case input_date_types[3]: //from_only
+            enable_date("from", how);
+            enable_date("to", how);
+            break;
+
+    }
+}
+
+    $(document).ready(function(){
+
+        //set init;
+        if (!$('#rerank_cb').attr("checked")) {
+            rerank_disable();
         }
-        rerank_disable("fade");
-    //Here do the stuff you want to do when 'unchecked'
-    });
+        //details hide
+        disable_all_rerank_details();
+
+        //setup date input;
+        my_date_input_extend();
+        $(".date_input").date_input();
+
+        //setup dates init
+        var init_date_type =  $('#input_date_type').val();
+        hide_show_dates(init_date_type); //fast
 
 
-    $('.rerankType').change(function () {
+
+
+        //----------functions----------------------
+
+        $('#input_date_type').change(function(){
+           hide_show_dates( $('#input_date_type').val(), "fade");
+           //alert("hi");
+
+        });
+  
+        $('#rerank_cb').change(function () {
+            if ($('#rerank_cb').attr("checked")) {
+                rerank_enable("fade");
+                return;
+            }
+            rerank_disable("fade");
+            //Here do the stuff you want to do when 'unchecked'
+        });
+
+
+        $('.rerankType').change(function () {
        
-        //disables and hides everything but actually selected!
-        disable_all_rerank_details("fade");
+            //disables and hides everything but actually selected!
+            disable_all_rerank_details("fade");
 
+        });
+
+
+        // -------------------------------------done--------------------
     });
-
-
-// -------------------------------------done--------------------
-});
 
 
